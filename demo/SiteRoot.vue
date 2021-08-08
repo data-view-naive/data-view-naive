526b8778dd5d4da93a81a8dceac263b2e2302558
成雨 <rensailong@zhongan.io>
2021-08-08 20:31:43 +0800

    feat: 删除n-theme-editor

--- demo/SiteRoot.vue
+++ demo/SiteRoot.vue
@@ -9,18 +9,16 @@
     :date-locale="dateLocale"
     :hljs="hljs"
   >
-    <n-theme-editor :style="themeEditorStyle">
-      <n-loading-bar-provider>
-        <n-message-provider>
-          <n-notification-provider>
-            <n-dialog-provider>
-              <Site />
-            </n-dialog-provider>
-          </n-notification-provider>
-        </n-message-provider>
-      </n-loading-bar-provider>
-      <n-global-style />
-    </n-theme-editor>
+    <n-loading-bar-provider>
+      <n-message-provider>
+        <n-notification-provider>
+          <n-dialog-provider>
+            <Site />
+          </n-dialog-provider>
+        </n-notification-provider>
+      </n-message-provider>
+    </n-loading-bar-provider>
+    <n-global-style />
   </component>
 </template>
 
